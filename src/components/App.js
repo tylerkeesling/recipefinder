e43d8d2087e25af559e28a0ff105d98d6aaa695e
tkeesling <tyler.keesling@gmail.com>
2017-10-11 21:27:13 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import SearchRecipes from './SearchRecipes'
+import RecipeList from './RecipeList'
+import '../styles/index.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Recipe Finder</h2>
+        <SearchRecipes />
+        <RecipeList />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import { connect } from 'react-redux'
+import { setRecipes } from '../actions'
+
+class SearchRecipes extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      ingredients: '',
+      dish: ''
+    }
+  }
+
+  search() {
+    let { ingredients, dish } = this.state
+    const url = `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=${ ingredients }&q=${ dish }`
+
+    fetch(url, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      this.props.setRecipes(json.results)
+    })
+  }
+
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+          <ControlLabel>Ingredients</ControlLabel>
+          {' '}
+          <FormControl
+            type='text'
+            placeholder='chicken, garlic'
+            onChange={ event => this.setState({ ingredients: event.target.value}) }
+          />
+        </FormGroup>
+        {' '}
+        <FormGroup>
+          <ControlLabel>Dish</ControlLabel>
+          {' '}
+          <FormControl
+            type='text'
+            placeholder='adobo'
+            onChange={ event => this.setState({ dish: event.target.value }) }
+          />
+        </FormGroup>
+        {' '}
+        <Button onClick={ () => this.search() }>Submit</Button>
+      </Form>
+    )
+  }
+}
+
+export default connect(null, { setRecipes })(SearchRecipes)
